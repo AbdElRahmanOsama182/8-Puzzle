a808d2379aabbfb5fa22980efefa1a2e801716d7
ahmedyoussefg <a.goerany@gmail.com>
2024-10-25 00:08:41 +0300

    Refactor A*

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -1,9 +1,7 @@
-from search_algorithm import SearchAlgorithm
+from algorithms.search_algorithm import SearchAlgorithm
 from puzzle.state_handler import StateHandler
 
 class AStar(SearchAlgorithm):
     def __init__(self, heuristics):
-        self.GOAL_STATE="012345678"
         self.heuristics=heuristics
-        self.state_handler = StateHandler()
-    
+        super().__init__()
