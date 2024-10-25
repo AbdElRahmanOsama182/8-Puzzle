a8062167cbc9df7e9b327f6fe551622e65f2ec7a
Bassam <abdelrahmanbassam0109@gmail.com>
2024-10-25 13:55:40 +0300

    quick optimization and show frontier_max_size

--- src/algorithms/iterative_dfs.py
+++ src/algorithms/iterative_dfs.py
@@ -7,7 +7,7 @@ class IterativeDFS(SearchAlgorithm):
 
     def search(self, initial_state):
         cur_iteration_depth = 0
-        
+        frontier_max_size = 0
         self.search_depth = 0
         self.path_to_goal = []
         self.states_to_goal = []
@@ -19,30 +19,32 @@ class IterativeDFS(SearchAlgorithm):
         while True:
             state = int(initial_state)
             frontier.append((0, state))
-            cost = {}
+            depths = {}
             parent[state] = -1
-            cost[state] = 0
+            depths[state] = 0
 
             while frontier:
-                depth, state = frontier.pop()
-                state_str = str(state)
+                frontier_max_size = max(frontier_max_size, len(frontier))
+                cur_depth, state = frontier.pop()
 
+                state_str = str(state)
 
                 if self.is_goal(state_str):
                     self.rebuild_path(parent)
-                    self.search_depth = depth
-                    print(cur_iteration_depth )
+                    self.search_depth = cur_depth
+                    print("frontier_max_size:", frontier_max_size)
+                    # print("number_of_nodes_expanded"+self.number_of_nodes_expanded)
                     return True
                 
-                if depth >= cur_iteration_depth: 
-                    continue
+                # if depth >= cur_iteration_depth: 
+                #     continue
 
                 self.number_of_nodes_expanded += 1
                 for child, dir in self.state_handler.get_children(state_str):
-                    if child not in cost or cost[child] > depth + 1:
-                        frontier.append((depth+1, child))
+                    if (child not in depths or depths[child] > cur_depth + 1) and cur_depth + 1 < cur_iteration_depth:
+                        frontier.append((cur_depth+1, child))
                         parent[child] = (state, dir)  
-                        cost[child] = depth + 1
+                        depths[child] = cur_depth + 1
             cur_iteration_depth += 1
        
         return False  
--- src/main.py
+++ src/main.py
@@ -6,6 +6,6 @@ from puzzle.search_result import SearchResult
 initial_state = "867254301"
 # game = PuzzleBoard("410263758", "a_star", "euclidean")
 # game = PuzzleBoard(initial_state, "iterative_dfs", None, "123456789")
-game = PuzzleBoard(initial_state, "iterative_dfs", None, "123456789")
+game = PuzzleBoard(initial_state, "bfs", None, "123456789")
 results:SearchResult = game.solve()
 print(results)
